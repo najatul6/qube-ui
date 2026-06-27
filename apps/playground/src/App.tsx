102ff064236fcb447e03058b9a5e640c01bc4298
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-06-27 23:13:49 +0600

    Upadate App.tsx

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -4,16 +4,19 @@ import { TapeMenu } from "@qube-ui/navigation";
 export default function App() {
   const [value, setValue] = useState("home");
 
+  const [show, setShow] = useState(true);
+
   return (
-    <TapeMenu
-      value={value}
-      onValueChange={setValue}
-    >
-      <TapeMenu.Item value="home">Home</TapeMenu.Item>
-      <TapeMenu.Item value="products">Products</TapeMenu.Item>
-      <TapeMenu.Item value="pricing">Pricing</TapeMenu.Item>
-      <TapeMenu.Item value="about">About</TapeMenu.Item>
-      <TapeMenu.Item value="contact">Contact</TapeMenu.Item>
-    </TapeMenu>
+    <>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+
+      <TapeMenu defaultValue="home">
+        <TapeMenu.Item value="home">Home</TapeMenu.Item>
+
+        {show && <TapeMenu.Item value="products">Products</TapeMenu.Item>}
+
+        <TapeMenu.Item value="pricing">Pricing</TapeMenu.Item>
+      </TapeMenu>
+    </>
   );
 }
